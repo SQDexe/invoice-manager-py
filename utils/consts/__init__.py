17c755eb5fc0de7cf1118b5b0869303402dd699c
Jakub Szuchniewicz <66796000+SQDexe@users.noreply.github.com>
2024-11-19 21:28:18 +0100

    Update __init__.py

--- utils/consts/__init__.py
+++ utils/consts/__init__.py
@@ -1,7 +1,6 @@
-from .consts import BAD_CHARS, MIN_DATE, ROMAN, Slice
+from .consts import BAD_CHARS, MIN_DATE, ROMAN
 __all__: list[str] = [
     'BAD_CHARS',
     'MIN_DATE',
-    'ROMAN',
-    'Slice'
+    'ROMAN'
     ]
