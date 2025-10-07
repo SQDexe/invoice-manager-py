a1d27bcc987d40a1ef63de3fe37b9b35c22f94ca
Jakub Szuchniewicz <66796000+SQDexe@users.noreply.github.com>
2025-10-07 12:00:56 +0200

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
-from .utils import Function, Name, Namespace, PrinterApp
+from .utils import Function, Name, Namespace, App
 from .consts import BAD_CHARS, MIN_DATE, ROMAN
 from .funcs import connect_dates, date2str, extract_dates, flatten, get_state, get_timespan_desc, pair_cross, pair_up, point2tuple, replace_mutiple, roman2int, sort2return, str2date
